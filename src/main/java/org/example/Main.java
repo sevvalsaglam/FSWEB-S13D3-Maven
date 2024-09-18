c043f16dd4a35f3deb0ffff8214f7f6fc2737d46
sevvalsaglam <sevvallsaglam@gmail.com>
2024-09-18 21:49:49 +0300

    sprint 13 gün 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+//Soru-1
+        Person person = new Person("John", "Doe", 20);
+
+        System.out.println("Firstname: " + person.getFirstName());
+
+        System.out.println("LastName: " + person.getLastName());
+
+        System.out.println("Age: " + person.getAge());
+
+        Person person2 =new Person("Şevval", "Sağlam", 26,"Female", 175, 52);
+        System.out.println("Firstname: " + person2.getFirstName());
+        System.out.println("LastName: " + person2.getLastName());
+        System.out.println("Age: " + person2.getAge());
+        System.out.println("Gender: " + person2.getGender());
+        System.out.println("Height: " + person2.getHeight());
+        System.out.println("Width: " + person2.getWidth());
+
+        System.out.println("---------------------------------------");
+
+//Soru-2
+        Wall wall = new Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
+        System.out.println("---------------------------------------");
+        wall.setHeight(12);
+        wall.setWidth(15);
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
+
     }
+
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class Person {
+
+    String firstName;
+    String lastName;
+    int age;
+    String gender;
+    int height;
+    int width;
+
+    public Person(String firsName,String lastName, int age){
+        this.firstName= firsName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+    public Person (String firsName, String lastName, int age,String gender, int height, int width){
+        this(firsName, lastName, age);
+        this.gender=gender;
+        this.height=height;
+        this.width=width;
+    }
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public String getGender() {
+        return this.gender;
+    }
+    public boolean isTeen() {
+        return this.age <= 19 && this.age >13;
+    }
+    public int getHeight() {
+        return this.height;
+    }
+    public int getWidth() {
+        return this.width;
+    }
+}
